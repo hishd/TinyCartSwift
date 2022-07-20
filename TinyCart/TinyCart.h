8ab4a63ab13e4e3a52f25934e1d5fe2b1e6d2fc2
Hishara Dilshan <hisharadilshan3@gmail.com>
2022-07-21 00:12:39 +0530

    Initial Commit

---
+++ TinyCart/TinyCart.h
@@ -0,0 +1,18 @@
+//
+//  TinyCart.h
+//  TinyCart
+//
+//  Created by Hishara Dilshan on 2022-07-21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TinyCart.
+FOUNDATION_EXPORT double TinyCartVersionNumber;
+
+//! Project version string for TinyCart.
+FOUNDATION_EXPORT const unsigned char TinyCartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TinyCart/PublicHeader.h>
+
+
